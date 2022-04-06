ab14c7d489206d33fc371b3553643cdd5b6f244a
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-04-06 15:04:04 -0400

    Added rules, gamemode select, welcome message, and a human class

--- game.py
+++ game.py
@@ -1,13 +1,33 @@
+from player import Player
+from human import Human
+from computer import Computer
+
 class Game:
     def __init__(self):
-        self.welcome()
+        self.welcome_message()
         self.rules()
-        self.gamemode()
-    def welcome(self):
-        pass
+        self.gamemode = self.pick_gamemode()
+
+    def welcome_message(self):
+        print('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
+
     def rules(self):
-        pass
-    def gamemode(self):
-        pass
+        print('The rules of the game are as follows:\nRock beats scissors and lizard\nPaper beats rock and spock\nScissors beats paper and lizard\nLizard beats spock and paper\nSpock beats scissors and rock')
+    
+    def pick_gamemode(self):
+        valid_input = 0
+        while valid_input == 0:
+            selected_gamemode = input('Please enter 1 for singleplayer or 2 for 1v1 multiplayer. ')
+            if selected_gamemode == '1':
+                print('Loading singleplayer!')
+                valid_input += 1
+                return selected_gamemode
+            elif selected_gamemode == '2':
+                print('Loading multiplayer!')
+                valid_input += 1
+                return selected_gamemode
+            else:
+                print('Sorry, input not recognized, please try again.')
     def gameplay(self):
         pass
+Game()
---
+++ human.py
@@ -0,0 +1,4 @@
+from player import Player
+class Human(Player):
+    def __init__(self):
+        super().__init__()

575b6872ac6a78341650bc8b1e5974ebd9db4efa
Jes94 <102390824+Jes94@users.noreply.github.com>
2022-04-06 12:53:58 -0400

    Updated game.py with foundation of the functions

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
-from computer import Computer
-from player import Player
 class Game:
-
     def __init__(self):
+        self.welcome()
+        self.rules()
+        self.gamemode()
+    def welcome(self):
+        pass
+    def rules(self):
+        pass
+    def gamemode(self):
         pass
